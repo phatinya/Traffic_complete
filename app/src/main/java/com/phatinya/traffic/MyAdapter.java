176abf6f99b407d50bce122767c28fb0c8bee130
phatinya <primmiekiki1994@gmail.com>
2016-09-28 15:10:29 +0700

    Initial commit

---
+++ app/src/main/java/com/phatinya/traffic/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.phatinya.traffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by user on 22/9/2559.
+ */
+
+public class MyAdapter extends BaseAdapter {
+    //explicit
+    private Context objContext;
+    private int[] trafficInts;
+    private String[] trafficStrings;
+
+    public MyAdapter(Context objContext, int[] trafficInts, String[] trafficStrings) {
+        this.objContext = objContext;
+        this.trafficInts = trafficInts;
+        this.trafficStrings = trafficStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return trafficStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+//Show Icon
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imvShowicon);
+        iconImageView.setBackgroundResource(trafficInts[i]);
+//Show Title
+        TextView titleTextView = (TextView) view1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(trafficStrings[i]);
+        return view1;
+    }
+}
