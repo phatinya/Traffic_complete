84e42c99aa495d744ef4514334fb8fc609777159
phatinya <primmiekiki1994@gmail.com>
2016-09-28 17:09:21 +0700

    link Activity

--- app/src/main/java/com/phatinya/traffic/MainActivity.java
+++ app/src/main/java/com/phatinya/traffic/MainActivity.java
@@ -1,10 +1,12 @@
 package com.phatinya.traffic;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -23,7 +25,28 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this,intIcon,strTitle);
         ListView myListView = (ListView) findViewById(R.id.listView);
         myListView.setAdapter(objMyAdapter);
+
+        //Activity When Click LisView
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                myIntentToDetail(i);
+
+            }
+        });
+
+
+
+
     } //createListView
+
+    private void myIntentToDetail (int intClick) {
+        Intent objIntent =new Intent(MainActivity.this, showDetailActivity.class);
+        objIntent.putExtra("click",intClick);
+        startActivity(objIntent);
+
+
+    }
     public void ClickAboutMe (View view){
         Intent objIntent = new Intent(Intent.ACTION_VIEW);
         objIntent.setData(Uri.parse("http://www.expertlab.ssru.ac.th"));
